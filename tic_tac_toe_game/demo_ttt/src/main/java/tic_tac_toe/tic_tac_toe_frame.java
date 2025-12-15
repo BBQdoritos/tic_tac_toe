e1f2215e7b63db0de9f2b9e6be52459297e8c2a1
Matthew Lawford <lawford.matthew@gmail.com>
2025-12-15 13:05:33 -0500

    Going into testing using JUnit, excited

--- tic_tac_toe_game/demo_ttt/src/main/java/tic_tac_toe/tic_tac_toe_frame.java
+++ tic_tac_toe_game/demo_ttt/src/main/java/tic_tac_toe/tic_tac_toe_frame.java
@@ -1,7 +1,7 @@
 package tic_tac_toe;
 
 import java.awt.*; 
-import java.awt.event.*;
+// import java.awt.event.*;
 import javax.swing.*;
 
 
--- tic_tac_toe_game/demo_ttt/src/main/java/tic_tac_toe/tic_tac_toe_logic.java
+++ tic_tac_toe_game/demo_ttt/src/main/java/tic_tac_toe/tic_tac_toe_logic.java
@@ -1,7 +1,12 @@
 package tic_tac_toe;
 
+import java.util.Scanner;
+
 public class tic_tac_toe_logic {
-    int startingPlayer = 0;
+    //user input
+    Scanner scanner = new Scanner(System.in);
+
+    int startingTurn = 0;
 
     //a 2D board, should be empty-pop
     String[][] board = new String[3][3];
@@ -11,7 +16,37 @@ public class tic_tac_toe_logic {
 
     //How do we get the specific tile that the user wants?
     
-    //allow player to put it if the tile is empty, and it is their turn
-    //how do we make sure it is their turn?
+    //allow player to put it if the tile is empty
+    //need an input here
+
+    /**
+     * Documentation of a Placement
+     * @param x is the row
+     * @param y is the column
+     * @return the coordinate that the player chose
+     */
+    record Placement (int x, int y) {}
+
+    Placement readMove () {
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        return new Placement(x, y);
+    }
+
+    Placement readMoveTesting(int x, int y) {
+        return new Placement(x,y);
+    } 
+
+    boolean turn (Placement placement) {
+        int row = placement.x();
+        int column = placement.y();
+
+        if (board[row][column] == null) {
+            board[row][column] = (startingTurn % 2 == 0) ? "O" : "X";
+            return true;
+        }
+        return false;
+    }
+
 
 }
